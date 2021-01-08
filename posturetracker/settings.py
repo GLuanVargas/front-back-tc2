ff36935a9b719d3f730d50440cfdaca5c72914e5
Yago Gehres <yagolgehres@gmail.com>
2021-01-07 22:56:23 -0300

    ok

--- posturetracker/settings.py
+++ posturetracker/settings.py
@@ -134,5 +134,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
+STATIC_URL = '/static'
